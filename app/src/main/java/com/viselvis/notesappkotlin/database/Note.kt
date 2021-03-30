87cb3098c2f6a22f35a07379526eb27d9fa1c433
theviselvis <visntr@gmail.com>
2021-03-30 20:44:14 +0800

    Complete basic flow

--- app/src/main/java/com/viselvis/notesappkotlin/database/Note.kt
+++ app/src/main/java/com/viselvis/notesappkotlin/database/Note.kt
@@ -5,9 +5,16 @@ import androidx.room.PrimaryKey
 
 @Entity (tableName = "note_table")
 data class Note (
-    @PrimaryKey(autoGenerate = true) var noteId: Long,
+    @PrimaryKey(autoGenerate = true) val noteId: Long,
     var noteTitle: String,
     var noteContent: String,
     var noteFirstCreated: Long,
     var noteLastModified: Long
-)
+) {
+    constructor(
+            noteTitle: String,
+            noteContent: String,
+            noteFirstCreated: Long,
+            noteLastModified: Long
+    ) : this(0, noteTitle, noteContent, noteFirstCreated, noteLastModified)
+}
